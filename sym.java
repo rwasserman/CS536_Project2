140b0de3455591659265407658546c09ff50415b
rwasserman <rwasserman2@wisc.edu>
2022-02-18 22:34:57 -0600

    Wrote some tokens. Check neededTokens for info.

--- sym.java
+++ sym.java
@@ -15,7 +15,7 @@ public class sym {
   public static final int INT = 3;
   public static final int LESS = 37;
   public static final int MINUS = 29;
-  public static final int WRITE = 24;
+  public static final int WRITE = 24; // <<
   public static final int RPAREN = 20;
   public static final int NOT = 32;
   public static final int SEMICOLON = 21;
@@ -29,7 +29,7 @@ public class sym {
   public static final int IF = 10;
   public static final int DOT = 23;
   public static final int ID = 14;
-  public static final int EOF = 0;
+  public static final int EOF = 0;  //Already done
   public static final int MINUSMINUS = 27;
   public static final int RETURN = 13;
   public static final int TRUE = 5;
@@ -41,7 +41,7 @@ public class sym {
   public static final int TIMES = 30;
   public static final int ELSE = 11;
   public static final int DISP = 9;
-  public static final int READ = 25;
+  public static final int READ = 25; // >>
   public static final int NOTEQUALS = 36;
   public static final int WHILE = 12;
   public static final int GREATEREQ = 40;
